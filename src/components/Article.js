086e215c79cc03f064e131da65d62b0531e5b202
obelix <bonacorc@etu.univ-grenoble-alpes.fr>
2022-12-05 00:34:02 +0100

    suppr of modification

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 const Article = (comment, updateArticles, messageModified) => {
 
     const [messageEditable, setMessageEditable] = useState(false);
+    const [newMessage, setNewMessage] = useState('');
     const deleteArticle = () => {
         axios.delete("http://localhost:3003/articles/" + comment.idMessage).then(() => { updateArticles() });
     }
@@ -24,46 +25,62 @@ const Article = (comment, updateArticles, messageModified) => {
         }
     );
 
-    if (!messageEditable) {
-        return (
+    return (
 
-            <div id="div-article">
-                <div id='article-entete'>
-                    <h3 id='h3-article'>{comment.author}</h3>
-                    <p><i>Posté le {jour} à {heure}</i></p>
-                </div>
-                <hr />
-                <p id='article-body'>{comment.message}</p>
-                <div id='article-boutons'>
-                    <button onClick={() => { modifArticle() }}>Modifier</button>
-                    <button onClick={() => { deleteArticle() }}>Supprimer</button>
-                </div>
+        <div id="div-article">
+            <div id='article-entete'>
+                <h3 id='h3-article'>{comment.author}</h3>
+                <p><i>Posté le {jour} à {heure}</i></p>
             </div>
-        );
-
-    } else {
-        return (
-            <div id="div-article">
-                <div id='article-entete'>
-                    <h3 id='h3-article'>{comment.author}</h3>
-                    <p><i>Posté le {jour} à {heure}</i></p>
-                </div>
-                <hr />
-                <textarea
-                    id='blog-textarea article-body'
-                    placeholder='Message'
-                    className='message-input'
-                    onChange={(element) => setNewMessage(element.target.value)}
-                    value={messageModified}
-                    required />
-                <div id='article-boutons'>
-                    <button onClick={() => { modifArticle() }}>Modifier</button>
-                    <button onClick={() => { deleteArticle() }}>Supprimer</button>
-                </div>
+            <hr />
+            <p id='article-body'>{comment.message}</p>
+            <div id='article-boutons'>
+                <button onClick={() => { modifArticle() }}>Modifier</button>
+                <button onClick={() => { deleteArticle() }}>Supprimer</button>
             </div>
-        );
-    }
-
+        </div>
+    );
+    /*
+        if (!messageEditable) {
+            return (
+    
+                <div id="div-article">
+                    <div id='article-entete'>
+                        <h3 id='h3-article'>{comment.author}</h3>
+                        <p><i>Posté le {jour} à {heure}</i></p>
+                    </div>
+                    <hr />
+                    <p id='article-body'>{comment.message}</p>
+                    <div id='article-boutons'>
+                        <button onClick={() => { modifArticle() }}>Modifier</button>
+                        <button onClick={() => { deleteArticle() }}>Supprimer</button>
+                    </div>
+                </div>
+            );
+    
+        } else {
+            return (
+                <div id="div-article">
+                    <div id='article-entete'>
+                        <h3 id='h3-article'>{comment.author}</h3>
+                        <p><i>Posté le {jour} à {heure}</i></p>
+                    </div>
+                    <hr />
+                    <textarea
+                        id='blog-textarea article-body'
+                        placeholder='Message'
+                        className='message-input'
+                        onChange={(element) => setNewMessage(element.target.value)}
+                        value={messageModified}
+                        required />
+                    <div id='article-boutons'>
+                        <button onClick={() => { modifArticle() }}>Enregistrer</button>
+                        <button onClick={() => { deleteArticle() }}>Supprimer</button>
+                    </div>
+                </div>
+            );
+        }
+    */
 
 
 };
